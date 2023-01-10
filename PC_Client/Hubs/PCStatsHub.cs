006bf73445d73637590168a4e6ce97a36b7b92ec
Brandon Trautmann <btrautmann@users.noreply.github.com>
2023-01-09 20:27:06 -0500

    initial commit

---
+++ PC_Client/Hubs/PCStatsHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PC_Client.Hubs
+{
+    public class PCStatsHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task HelloFromFlutter(string message)
+        {
+            await Clients.All.SendAsync("MessageFromFlutter", message);
+        }   
+    }
+}
---
+++ android/app/src/main/kotlin/com/example/pc_stats/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pc_stats
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
